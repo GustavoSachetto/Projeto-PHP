bf0ff4b870cffefe8e6333f4d7c0cc941489f14c
GustavoSachetto <g.sachettocruz@gmail.com>
2023-08-25 14:31:24 -0300

    Alterando atividade dolar.php

--- controller/dolar.php
+++ controller/dolar.php
@@ -16,11 +16,9 @@
             <section>
                 <?php  
 
-                    $url = 'https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoDolarPeriodo(dataInicial=@dataInicial,dataFinalCotacao=@dataFinalCotacao)?@dataInicial=\'08-02-2023\'&@dataFinalCotacao=\'08-16-2023\'&$top=1&$orderby=dataHoraCotacao%20desc&$format=json&$select=cotacaoCompra,dataHoraCotacao';
+                    
 
-                    $dados = json_decode(file_get_contents($url), true);
-
-                    $cotacao = $dados["value"][0]["cotacaoCompra"];
+                    $cotacao = 5.20;
 
                     $real = $_REQUEST["txtreal"] ?? 0;
 
